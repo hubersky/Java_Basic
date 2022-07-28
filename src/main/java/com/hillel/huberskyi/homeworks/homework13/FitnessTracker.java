69fcd573dc047560c8fa5ec323dd0a892b951f9d
gubersky <https://github.com/gubersky/Burger.git>
2022-07-28 20:38:07 +0300

    HomeWork13

---
+++ src/main/java/com/hillel/huberskyi/homeworks/homework13/FitnessTracker.java
@@ -0,0 +1,101 @@
+package com.hillel.huberskyi.homeworks.homework13;
+
+public class FitnessTracker {
+
+    private String name;
+    private int day;
+    private int month;
+    private int year;
+    private int age;
+    private String email;
+    private String phone;
+    private String surname;
+    private double weight;
+    private int pressure;
+    private int numberOfStepsPerDay;
+
+    public FitnessTracker(String name, int day, int month, int year, String email, String phone, String surname,
+                          double weight, int pressure, int numberOfStepsPerDay) {
+        this.name = name;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.age = countAge();
+        this.email = email;
+        this.phone = phone;
+        this.surname = surname;
+        this.weight = weight;
+        this.pressure = pressure;
+        this.numberOfStepsPerDay = numberOfStepsPerDay;
+    }
+
+    protected void printAccountInfo() {
+        System.out.println(
+                "name='" + name + '\'' +
+                        ", day=" + day +
+                        ", month=" + month +
+                        ", year=" + year +
+                        ", age=" + age +
+                        ", email='" + email + '\'' +
+                        ", phone='" + phone + '\'' +
+                        ", surname='" + surname + '\'' +
+                        ", weight=" + weight +
+                        ", pressure=" + pressure +
+                        ", numberOfStepsPerDay=" + numberOfStepsPerDay);
+
+    }
+
+    private int countAge() {
+        int currentYear = 2022;
+        int result = currentYear - this.year;
+       return this.age = result;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
+    public void setNumberOfStepsPerDay(int numberOfStepsPerDay) {
+        this.numberOfStepsPerDay = numberOfStepsPerDay;
+    }
+}
---
+++ src/main/java/com/hillel/huberskyi/homeworks/homework13/Main.java
@@ -0,0 +1,16 @@
+package com.hillel.huberskyi.homeworks.homework13;
+
+public class Main {
+    public static void main(String[] args) {
+        FitnessTracker firstUser = new FitnessTracker("Alex",10,7,1993,"alex.@gmail.com",
+                "+380973554533","Kotikov",66.8,120,1567);
+        FitnessTracker secondUser = new FitnessTracker("Max",22,11,2000,"max.@gmail.com",
+                "+380885637627","Verstappen",71.2,110,2988);
+        FitnessTracker lastUser = new FitnessTracker("Sebastian", 7,8,1997,"sebastian.@gmail.com",
+                "+380997563553","Vettel",75.0,125,4567);
+        firstUser.printAccountInfo();
+        secondUser.printAccountInfo();
+        lastUser.printAccountInfo();
+
+    }
+}
